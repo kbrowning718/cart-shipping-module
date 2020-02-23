28897c3163832e1c0d90349205a265425eef012c
Kirby Browning <kirby.browning718@gmail.com>
2020-02-23 00:47:02 -0500

    JS fixes

--- index.js
+++ index.js
@@ -83,8 +83,7 @@ for (var k = 0; k < notActive.length; k++) {
 
   clickedButton = e.target;
 if(clickedButton.classList.contains('feedback-button')) {
-
-    clickedButton.classList.add("clicked-feedback");
+clickedButton.classList.add("clicked-feedback");
   } else if(clickedButton.classList.contains('feedback-label')) {
     clickedButton.parentElement.classList.add('clicked-feedback');
   }
