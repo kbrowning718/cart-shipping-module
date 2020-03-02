a7f0b27f0a4a23a4bbcceb9664ec4b49d1e1b6f9
Kirby Browning <kirby.browning718@gmail.com>
2020-03-01 20:32:50 -0500

    JS add tool tip button fix

--- index.js
+++ index.js
@@ -93,6 +93,10 @@ for (var j = 0; j < feedbackButton.length; j++) {
         clickedButton.parentElement.classList.add('clicked-feedback');
       }
 
+      if(clickedButton.nextElementSibling.classList.contains('clicked-feedback')) {
+        clickedButton.nextElementSibling.classList.remove('clicked-feedback');
+      }
+
   });
 
 }
